ac283b0c9fc6517dce1f57665b7412b197d1c802
HemanthRaj0C <personalhemanthraj@gmail.com>
2026-02-03 10:48:55 +0530

    [Fix] URL

--- src/app/user/rules/page.tsx
+++ src/app/user/rules/page.tsx
@@ -208,7 +208,7 @@ export default function RulesPage() {
               <div className="flex flex-wrap gap-4">
                 {/* VM Download Button */}
                 <a
-                  href="https://drive.google.com/drive/folders/1SQd7NzH-KU8KcBEX1XGBw-6IiYDeX-Uy?usp=sharing"
+                  href="https://drive.google.com/file/d/1nH1iAwu5QGt-0UOQEJSyJuO0H4gJGD5r/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="group/btn relative px-6 py-3 text-sm rounded-md bg-neutral-800/50 font-medium text-white shadow-[0px_1px_1px_1px_#ffffff40_inset,0px_0px_0px_0px_#ffffff40_inset] transition-all cursor-pointer font-mono flex items-center gap-2 hover:bg-neutral-700/50"
@@ -220,7 +220,7 @@ export default function RulesPage() {
                 
                 {/* Local Server Download Button */}
                 <a
-                  href="http://172.16.14.197:8080/Ubutu-22%20%28Revil%29%201.ova"
+                  href="http://192.168.137.160:8080/Ubutu-22%20%28Revil%29%201.ova"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="group/btn relative px-6 py-3 text-sm rounded-md bg-neutral-800/50 font-medium text-white shadow-[0px_1px_1px_1px_#ffffff40_inset,0px_0px_0px_0px_#ffffff40_inset] transition-all cursor-pointer font-mono flex items-center gap-2 hover:bg-neutral-700/50"
